994f5d8cdf5ef8b57cf523458fc4f0efcfab08c9
Jocelio <joceliotecinfor@gmail.com>
2022-08-17 09:18:48 -0300

    Projeto Final com mudança

--- resources/views/listagem.blade.php
+++ resources/views/listagem.blade.php
@@ -4,10 +4,11 @@
     <div class="container-fluid bg-secondary px-5 d-none d-lg-block">
         <div class="row gx-0">
             <div class="col-lg-8 text-center text-lg-start mb-2 mb-lg-0">
-                <div class="d-inline-flex align-items-center" style="height: 45px;">
-                    <small class="me-3 text-light"></i>Governo Do Estado Do Ceará -> Acesso restrito para : &nbsp {{auth()->user()->name }}<b
-                    style="text-transform: uppercase;"></b></small>
-                </div>
+            <div class="d-inline-flex align-items-center" style="height: 45px;">
+                <small class="me-3 text-light"></i>Governo Do Estado Do Ceará<b
+                style="text-transform: uppercase;"></b></small>
+                <small class="me-3 text-light"><i class="fa fa-map-marker-alt me-2"></i>Seja bem vindo(a) {{auth()->user()->name }}</small>
+             </div>
             </div>
             <div class="col-lg-4 text-center text-lg-end">
                 <div class="d-inline-flex align-items-center" style="height: 45px;">
